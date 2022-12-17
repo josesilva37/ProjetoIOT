dc9dd9a38adb7c5e74436ebbd8194045e4098416
José Silva <63546835+josesilva37@users.noreply.github.com>
2022-12-17 11:50:15 +0000

    changes

--- receive.py
+++ receive.py
@@ -1,15 +1,18 @@
 import pika, sys, os
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    credentials = pika.PlainCredentials("jose", "qwerty")
+    host = "192.168.229.159"
+    parameters = pika.ConnectionParameters(host, 5672, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='pedometer')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='pedometer', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- send.py
+++ send.py
@@ -3,15 +3,14 @@ import pika
 
 #Read BLE Data
 
-
-
 #Send BLE Data to Broker
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='pedometer')
+channel.exchange_declare(exchange='data', exchange_type='fanout')
+# channel.queue_declare(queue='pedometer')
 
-channel.basic_publish(exchange='', routing_key='pedometer', body='Hello World!')
+channel.basic_publish(exchange='', routing_key='pedometer', body='Test')
 print(" [x] Sent 'Hello World!'")
 connection.close()
