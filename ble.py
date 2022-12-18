02f3dcb3ae44000aa239541ae34a8ccf0476ddf1
José Silva <63546835+josesilva37@users.noreply.github.com>
2022-12-18 16:02:09 +0000

    bleak

--- ble.py
+++ ble.py
@@ -18,11 +18,9 @@ from bleak.backends.characteristic import BleakGATTCharacteristic
 
 
 # you can change these to match your device or override them from the command line
-CHARACTERISTIC_UUID = ""
+CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 ADDRESS = (
     "C4:49:51:F3:54:F0"
-    if platform.system() != "Darwin"
-    else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
 )
 
 
@@ -34,14 +32,18 @@ def notification_handler(characteristic: BleakGATTCharacteristic, data: bytearra
 async def main(address, char_uuid):
     async with BleakClient(address) as client:
         print(f"Connected: {client.is_connected}")
-
-        await client.start_notify(char_uuid, notification_handler)
+        # print(await client.read_gatt_descriptor("0x7fa5ac5147c0"))
+        # print(client.services.)
+        
+        # print(await client.read_gatt_char("6e400003-b5a3-f393-e0a9-e50e24dcca9e"))
+        await client.start_notify(char_uuid, notificatio0n_handler)
         await asyncio.sleep(5.0)
         await client.stop_notify(char_uuid)
 
 
 if __name__ == "__main__":
     asyncio.run(
+        # main(ADDRESS, CHARACTERISTIC_UUID)
         main(
             sys.argv[1] if len(sys.argv) > 1 else ADDRESS,
             sys.argv[2] if len(sys.argv) > 2 else CHARACTERISTIC_UUID,
--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ from bleak import _logger as logger
 
 
 CHARACTERISTIC_UUID = (
-    '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+    '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 )  # <--- Change to the characteristic you want to enable notifications from.
 
 
